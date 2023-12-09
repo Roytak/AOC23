713159735db94e703067922ba73f55a2b19be8a5
Roytak <xjanot04@fit.vutbr.cz>
2023-12-09 12:52:08 +0100

    day nine

--- 9/9a.c
+++ 9/9a.c
@@ -3,11 +3,60 @@
 #include <stdlib.h>
 #include <string.h>
 
+#define NINPUTS 21
+
+int
+parse(int *input, int size)
+{
+	int next[NINPUTS];
+	int i;
+
+	/* check if we should end */
+	for (i = 0; i < size; i++) {
+		if (input[i]) {
+			break;
+		}
+	}
+	if (i == size) {
+		return 0;
+	}
+
+	/* create the next array */
+	for (i = 0; i < size - 1; i++) {
+		next[i] = input[i + 1] - input[i];
+	}
+
+	return input[size - 1] + parse(next, size - 1);
+}
+
 int
 main(void)
 {
 	FILE *f = fopen("in.txt", "r");
+	int input[NINPUTS];
+	int i, sum = 0;
+	char line[512] = {0};
+	char *ptr;
 
+	while (fgets(line, 512, f)) {
+		ptr = line;
+		for (i = 0; i < NINPUTS; i++) {
+			sscanf(ptr, "%d", &input[i]);
+
+			while (*ptr != '\0' && (*ptr < '0' || *ptr > '9')) {
+                ptr++;
+            }
+
+            while (*ptr != '\0' && *ptr >= '0' && *ptr <= '9') {
+                ptr++;
+            }
+		}
+
+		sum += parse(input, NINPUTS);
+		memset(line, 0, 512);
+	}
+
+	printf("%d\n", sum);
 	return 0;
 }
 
