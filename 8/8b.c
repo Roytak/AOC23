bb115cc2dc596db22b686983f924c9d53a8ae5da
Roytak <xjanot04@fit.vutbr.cz>
2023-12-09 13:01:35 +0100

    day eight

--- 8/8b.c
+++ 8/8b.c
@@ -11,8 +11,8 @@ struct node {
 };
 
 struct end {
-	int *steps;
-	int nsteps;
+    int *steps;
+    int nsteps;
 };
 
 char directions[512];
@@ -46,21 +46,31 @@ get_direction()
     return d;
 }
 
+// Function to find the GCD (Greatest Common Divisor)
+unsigned long long gcd(unsigned long long a, unsigned long long b) {
+    while (b != 0) {
+        unsigned long long temp = b;
+        b = a % b;
+        a = temp;
+    }
+    return a;
+}
+
+// Function to find the LCM (Least Common Multiple)
+unsigned long long lcm(unsigned long long a, unsigned long long b) {
+    return (a * b) / gcd(a, b);
+}
+
 int
 main(void)
 {
     FILE *f = fopen("in.txt", "r");
     struct node *nodes = NULL;
-    int nnodes = 0, i, sum = 0;
-    unsigned long j;
+    int nnodes = 0, i;
     char buffer[512] = {0}, left[4] = {0}, right[4] = {0};
     char *ptr;
     struct node *iter1, *iter2, *iter3, *iter4, *iter5, *iter6;
     char d;
-    struct node **starts = NULL;
-    int nstarts = 0;
-    struct end *ends;
-    int found = 0;
 
     fgets(directions, 512, f);
     fgets(buffer, 512, f);
@@ -85,154 +95,192 @@ main(void)
 
     iter1 = iter2 = iter3 = iter4 = iter5 = iter6 = NULL;
     for (i = 0; i < nnodes; i++) {
-    	if (nodes[i].name[2] == 'A') {
-    		if (!iter1) {
-    			iter1 = &nodes[i];
-    		} else if (!iter2) {
-    			iter2 = &nodes[i];
-    		} else if (!iter3) {
-    			iter3 = &nodes[i];
-    		} else if (!iter4) {
-    			iter4 = &nodes[i];
-    		} else if (!iter5) {
-    			iter5 = &nodes[i];
-    		} else {
-    			iter6 = &nodes[i];
-    		}
-    	}
+        if (nodes[i].name[2] == 'A') {
+            if (!iter1) {
+                iter1 = &nodes[i];
+            } else if (!iter2) {
+                iter2 = &nodes[i];
+            } else if (!iter3) {
+                iter3 = &nodes[i];
+            } else if (!iter4) {
+                iter4 = &nodes[i];
+            } else if (!iter5) {
+                iter5 = &nodes[i];
+            } else {
+                iter6 = &nodes[i];
+            }
+        }
     }
     int one, two, three, four, five, six;
-    int first = 0, second;
+    int first = 0, second = 0;
+    int match = 0;
     i = 0;
     while (1) {
-    	if (iter1->name[2] == 'Z') {
-    		if (!first) {
-    			first = i;
-    		} else {
-    			second = i;
-    			break;
-    		}
-    	}
-    	d = get_direction();
-    	if (d == 'L') {
-    		iter1 = iter1->left;
-    	} else {
-    		iter1 = iter1->right;
-    	}
-    	i++;
+        if (iter1->name[2] == 'Z') {
+            if (!first) {
+                first = i;
+            } else {
+                second = i;
+                break;
+            }
+            match++;
+        }
+        d = get_direction();
+        if (d == 'L') {
+            iter1 = iter1->left;
+        } else {
+            iter1 = iter1->right;
+        }
+        i++;
+        if (match == 8) {
+            break;
+        }
     }
-   	one = second - first;
+    printf("\n");
+    one = second - first;
     i = 0;
     first = second = 0;
+    match = 0;
+    current = 0;
     while (1) {
-    	if (iter2->name[2] == 'Z') {
-    		if (!first) {
-    			first = i;
-    		} else {
-    			second = i;
-    			break;
-    		}
-    	}
-    	d = get_direction();
-    	if (d == 'L') {
-    		iter2 = iter2->left;
-    	} else {
-    		iter2 = iter2->right;
-    	}
-    	i++;
+        if (iter2->name[2] == 'Z') {
+            if (!first) {
+                first = i;
+            } else {
+                second = i;
+                break;
+            }
+            match++;
+        }
+        d = get_direction();
+        if (d == 'L') {
+            iter2 = iter2->left;
+        } else {
+            iter2 = iter2->right;
+        }
+        i++;
+        if (match == 10) {
+            break;
+        }
     }
+    printf("\n");
     two = second - first;
     i = 0;
     first = second = 0;
+    match = 0;
+    current = 0;
     while (1) {
-    	if (iter3->name[2] == 'Z') {
-    		if (!first) {
-    			first = i;
-    		} else {
-    			second = i;
-    			break;
-    		}
-    	}
-    	d = get_direction();
-    	if (d == 'L') {
-    		iter3 = iter3->left;
-    	} else {
-    		iter3 = iter3->right;
-    	}
-    	i++;
+        if (iter3->name[2] == 'Z') {
+            if (!first) {
+                first = i;
+            } else {
+                second = i;
+                break;
+            }
+            match++;
+        }
+        d = get_direction();
+        if (d == 'L') {
+            iter3 = iter3->left;
+        } else {
+            iter3 = iter3->right;
+        }
+        i++;
+        if (match == 10) {
+            break;
+        }
     }
+    printf("\n");
     three = second - first;
     i = 0;
     first = second = 0;
+    match = 0;
+    current = 0;
     while (1) {
-    	if (iter4->name[2] == 'Z') {
-    		if (!first) {
-    			first = i;
-    		} else {
-    			second = i;
-    			break;
-    		}
-    	}
-    	d = get_direction();
-    	if (d == 'L') {
-    		iter4 = iter4->left;
-    	} else {
-    		iter4 = iter4->right;
-    	}
-    	i++;
+        if (iter4->name[2] == 'Z') {
+            if (!first) {
+                first = i;
+            } else {
+                second = i;
+                break;
+            }
+            match++;
+        }
+        d = get_direction();
+        if (d == 'L') {
+            iter4 = iter4->left;
+        } else {
+            iter4 = iter4->right;
+        }
+        i++;
+        if (match == 10) {
+            break;
+        }
     }
+    printf("\n");
     four = second - first;
     i = 0;
     first = second = 0;
+    match = 0;
+    current = 0;
     while (1) {
-    	if (iter5->name[2] == 'Z') {
-    		if (!first) {
-    			first = i;
-    		} else {
-    			second = i;
-    			break;
-    		}
-    	}
-    	d = get_direction();
-    	if (d == 'L') {
-    		iter5 = iter5->left;
-    	} else {
-    		iter5 = iter5->right;
-    	}
-    	i++;
+        if (iter5->name[2] == 'Z') {
+            if (!first) {
+                first = i;
+            } else {
+                second = i;
+                break;
+            }
+            match++;
+        }
+        d = get_direction();
+        if (d == 'L') {
+            iter5 = iter5->left;
+        } else {
+            iter5 = iter5->right;
+        }
+        i++;
+        if (match == 10) {
+            break;
+        }
     }
+    printf("\n");
     five = second - first;
     i = 0;
     first = second = 0;
+    match = 0;
+    current = 0;
     while (1) {
-    	if (iter6->name[2] == 'Z') {
-    		if (!first) {
-    			first = i;
-    		} else {
-    			second = i;
-    			break;
-    		}
-    	}
-    	d = get_direction();
-    	if (d == 'L') {
-    		iter6 = iter6->left;
-    	} else {
-    		iter6 = iter6->right;
-    	}
-    	i++;
+        if (iter6->name[2] == 'Z') {
+            if (!first) {
+                first = i;
+            } else {
+                second = i;
+                break;
+            }
+            match++;
+        }
+        d = get_direction();
+        if (d == 'L') {
+            iter6 = iter6->left;
+        } else {
+            iter6 = iter6->right;
+        }
+        i++;
+        if (match == 10) {
+            break;
+        }
     }
     six = second - first;
 
-    unsigned long long ans = one * two * three *four * five *six;
+    unsigned long long ans;
+    ans = lcm(one, two);
+    ans = lcm(ans, three);
+    ans = lcm(ans, four);
+    ans = lcm(ans, five);
+    ans = lcm(ans, six);
 
     printf("%llu\n", ans);
-
-    // for (i = 0; i < nstarts; i++) {
-    // 	for (j = 0; j < ends[i].nsteps; j++) {
-    // 		printf("%d ", ends[i].steps[j]);
-    // 	}
-    // 	printf("\n");
-    // }
 
     return 0;
 }
